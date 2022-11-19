885d884c6f7acdd566db66bb217cc277c23c5c75
Gregory Edison <gregory.edison1993@gmail.com>
2022-11-19 17:36:04 +0100

    feat: types

--- src/infra/mongo/types.rs
+++ src/infra/mongo/types.rs
@@ -1,1 +1,15 @@
+use serde::{Deserialize, Serialize};
 
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Ingredient {
+    pub name: String,
+    // TODO change from string to hex string
+    pub hash: String,
+    // TODO change from string to hex string
+    pub path: Vec<String>,
+}
+
+#[derive(Debug, Deserialize, Serialize)]
+pub struct Recipe {
+    pub ingredients: Vec<Ingredient>,
+}
