25de8950138b733c6c1a1955340b9bdc60d9dd2f
charlesdelab <aymericdelabrousse@gmail.com>
2022-11-19 17:31:27 +0100

    feat: routes

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,37 @@
+use rocket::http::Status;
+
 mod infra;
 
 #[macro_use]
 extern crate rocket;
 
-#[get("/")]
-fn index() -> &'static str {
-    "Hello, world!"
+#[get("/ingredient/<id>")]
+fn get_ingredient(db: &State<MongoRepo>, id: String) -> Result<Json<Ingredient>, Status> {
+    if id.is_empty() {
+        return Err(Status::BadRequest);
+    };
+    let result = db.getIngredient(&id);
+
+    match result {
+        Ok(ingredient) => Ok(Json(ingredient)),
+        Err(_) => Err(Status::InternalServerError),
+    }
+}
+
+#[get("/recipes/<ids>")]
+fn get_recipes(db: &State<MongodRepo>, ids: Vec<String>) -> Result<Vec<Json<Recipe>>, Status> {
+    let result = db.get_recipes(ingredients);
+
+    match result {
+        Ok(recipes) => Ok(Json(recipes)),
+        Err(_) => Err(Status::InternalServerError),
+    }
 }
 
 #[launch]
 fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+    let db = MongoRepo::init();
+    rocket::build()
+        .manage(db)
+        .mount("/", routes![get_ingredient, get_recipes])
 }
