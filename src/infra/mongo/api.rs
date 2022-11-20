6273b049b40d6cd92fa741c1e586803a334139e2
Gregory Edison <gregory.edison1993@gmail.com>
2022-11-20 15:12:08 +0100

    test: mongo

--- src/infra/mongo/api.rs
+++ src/infra/mongo/api.rs
@@ -17,9 +17,9 @@ pub struct MongoRep {
 }
 
 impl MongoRep {
-    fn init(uri: String, data: &str) -> Result<Self, Box<dyn Error>> {
+    pub fn init(uri: String, database: &str) -> Result<Self, MongoRepError> {
         let client = Client::with_uri_str(uri)?;
-        let database = client.database(data);
+        let database = client.database(database);
         let rep = MongoRep {
             ingredients: database.collection("ingredients"),
             recipes: database.collection("recipes"),
@@ -27,14 +27,44 @@ impl MongoRep {
         return Ok(rep);
     }
 
-    fn get_ingredient(self, name: String) -> Result<Ingredient, MongoRepError> {
+    pub fn get_ingredient(&self, name: String) -> Result<Ingredient, MongoRepError> {
         match self
             .ingredients
-            .find_one(doc! {"name": &name}, None)
+            .find_one(doc! {"domain": &name}, None)
             .map_err(MongoRepError::from)?
         {
             Some(ing) => Ok(ing),
             _ => Err(MongoRepError::InvalidIngredientName(name)),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    fn init_repo(database: &str) -> MongoRep {
+        MongoRep::init(String::from("mongodb://localhost:27017/"), database).unwrap()
+    }
+
+    #[test]
+    fn test_init_mongo_repo_passes() {
+        init_repo("test");
+    }
+
+    #[test]
+    #[should_panic(expected = "InvalidIngredientName")]
+    fn test_get_ingredient_invalid_ingredient_query() {
+        let mongo_rep = init_repo("lfb");
+        mongo_rep.get_ingredient(String::from("hello.eth")).unwrap();
+    }
+
+    #[test]
+    fn test_get_ingredient_passes() {
+        let mongo_rep = init_repo("lfb");
+        let ingredient = mongo_rep
+            .get_ingredient(String::from("abricot.eth"))
+            .unwrap();
+        assert_eq!(ingredient.domain, "abricot.eth");
     }
 }
